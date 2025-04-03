186f9b6b04a312a0c5be7115ac07c9e0dc71f255
ecaruso42 <eugenio.caruso99@gmail.com>
2025-04-03 17:35:39 +0200

    struttura

--- cpp08/ex02/MutantStack.hpp
+++ cpp08/ex02/MutantStack.hpp
@@ -2,13 +2,21 @@
 
 #include <iostream>
 #include <stack>
+#include <deque>
 
-template<typename T>
+template<typename T, typename Container = std::deque<T> >
 
 class MutantStack : std::stack<T>{
 	public:
-		MutantStack();
-		MutantStack(const MutantStack& other);
-		MutantStack& operator=(const MutantStack& other);
-		~MutantStack();
+		MutantStack(){}
+		MutantStack(const MutantStack& other){
+			if (*this != other)
+				*this = std::stack<T, Container>::operator=(other);
+		}
+		MutantStack& operator=(const MutantStack& other){
+			if (*this != other)
+				this->c = other.c;
+			return *this;
+		}
+		~MutantStack(){}
 };
